685b4630ba5255578bb1fde73410ef91f5e98668
anazcom <tonito.nazco@gmail.com>
2025-11-27 02:45:07 -0500

    (install) added opt home to be created

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 XDG_CONFIG_HOME="$HOME/.config"
 XDG_BIN_HOME="$HOME/.local/bin"
+XDG_OPT_HOME="$HOME/.local/opt"
 SCRIPT_ROOT_DIR=$(dirname "$(realpath "$0")")
 
 mkdir -p $XDG_CONFIG_HOME
 mkdir -p $XDG_BIN_HOME
+mkdir -p $XDG_OPT_HOME
 
 echo "Executing script"
 mv "$SCRIPT_ROOT_DIR/tmux" "$XDG_CONFIG_HOME/tmux" 
