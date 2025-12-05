fb7be65597e4d2b39cc6056d933803aef096a45a
anazcom <tonito.nazco@gmail.com>
2025-12-05 06:23:30 -0500

    install: udpated the install to do not link some files based on configuration

--- install.sh
+++ install.sh
@@ -9,16 +9,18 @@ mkdir -p $XDG_CONFIG_HOME
 mkdir -p $XDG_BIN_HOME
 mkdir -p $XDG_OPT_HOME
 
-echo "Executing script"
-
+ln -s "$SCRIPT_ROOT_DIR/.bash_profile" "$HOME/.bash_profile" 
 ln -s "$SCRIPT_ROOT_DIR/.bashrc" "$HOME/.bashrc" 
 ln -s "$SCRIPT_ROOT_DIR/.vimrc" "$HOME/.vimrc" 
 
 ln -s "$SCRIPT_ROOT_DIR/tmux" "$XDG_CONFIG_HOME" 
 ln -s "$SCRIPT_ROOT_DIR/git" "$XDG_CONFIG_HOME" 
-ln -s "$SCRIPT_ROOT_DIR/hypr" "$XDG_CONFIG_HOME" 
 ln -s "$SCRIPT_ROOT_DIR/ghostty" "$XDG_CONFIG_HOME" 
-ln -s "$SCRIPT_ROOT_DIR/waybar" "$XDG_CONFIG_HOME" 
 
-ln -s "$SCRIPT_ROOT_DIR/code/settings.json" "$XDG_CONFIG_HOME/Code/User/settings.json" 
-ln -s "$SCRIPT_ROOT_DIR/code/keybindings.json" "$XDG_CONFIG_HOME/Code/User/keybindings.json" 
+
+if ! [[ "$(uname -s)" == "Darwin" ]]; then
+		ln -s "$SCRIPT_ROOT_DIR/code/settings.json" "$XDG_CONFIG_HOME/Code/User/settings.json" 
+		ln -s "$SCRIPT_ROOT_DIR/code/keybindings.json" "$XDG_CONFIG_HOME/Code/User/keybindings.json" 
+		ln -s "$SCRIPT_ROOT_DIR/waybar" "$XDG_CONFIG_HOME" 
+		ln -s "$SCRIPT_ROOT_DIR/hypr" "$XDG_CONFIG_HOME" 
+fi
