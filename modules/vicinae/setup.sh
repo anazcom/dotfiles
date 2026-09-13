1670b375d4b09b69882d3571efaf547127ca7b8b
anazcom <tonito.nazco@gmail.com>
2026-09-13 08:05:26 -0400

    added vicinae for spotlighting

---
+++ modules/vicinae/setup.sh
@@ -0,0 +1,14 @@
+require DOTFILES_ROOT_DIR
+require DOTFILES_CONFIG_DIR
+
+CONTENT_DIR="$DOTFILES_ROOT_DIR/modules/vicinae/content"
+
+if ! command -v vicinae >/dev/null 2>&1; then
+    warning "Vicinae is not installed. Skipping setup."
+    return 0
+fi
+
+rm -rf "$DOTFILES_CONFIG_DIR/vicinae/settings.json"
+ln -s "$CONTENT_DIR/settings.json" "$DOTFILES_CONFIG_DIR/vicinae/settings.json"
+
+success "Vicinae setup completed."
