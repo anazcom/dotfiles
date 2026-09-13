04565ada5a4b2a27ed0ebb108de88b0116881ae6
anazcom <tonito.nazco@gmail.com>
2026-09-13 11:45:03 -0400

    updated vicinae config

--- modules/vicinae/setup.sh
+++ modules/vicinae/setup.sh
@@ -8,7 +8,7 @@ if ! command -v vicinae >/dev/null 2>&1; then
     return 0
 fi
 
-rm -rf "$DOTFILES_CONFIG_DIR/vicinae/settings.json"
-ln -s "$CONTENT_DIR/settings.json" "$DOTFILES_CONFIG_DIR/vicinae/settings.json"
+rm -rf "$DOTFILES_CONFIG_DIR/vicinae"
+ln -s "$CONTENT_DIR" "$DOTFILES_CONFIG_DIR/vicinae"
 
 success "Vicinae setup completed."
